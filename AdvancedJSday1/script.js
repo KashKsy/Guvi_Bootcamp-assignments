7c1e7d4fa4edaf3e50070ec65590ebb7b33b0058
KashKsy <71564393+KashKsy@users.noreply.github.com>
2020-11-30 23:13:58 +0530

    Add files via upload

---
+++ AdvancedJSday1/script.js
@@ -0,0 +1,36 @@
+setTimeout(function(){
+    setTimeout(function(){
+        document.write(1);
+        document.write("Happy Independence Day!");
+    }, 10000)
+    setTimeout(function(){
+        document.write(2);
+    
+    }, 9000)
+    setTimeout(function(){
+        document.write(3);
+    }, 8000)
+    setTimeout(function(){
+        document.write(4);
+    }, 7000)
+    setTimeout(function(){
+        document.write(5);
+    }, 6000)
+    setTimeout(function(){
+        document.write(6);
+    
+    }, 5000)
+    setTimeout(function(){
+        document.write(7);
+    }, 4000)
+    setTimeout(function(){
+        document.write(8);
+    }, 3000)
+    setTimeout(function(){
+        document.write(9);
+    }, 2000)
+    setTimeout(function(){
+        document.write(10);
+    }, 1000)
+    
+    },0)
